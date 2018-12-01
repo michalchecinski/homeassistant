3d3042bd82f8f314149dfe220380450aa365ea12
Michał Chęciński <michal.checinski@outlook.com>
2018-12-01 08:58:46 +0100

    change update ha script

--- bash/update-ha.sh
+++ bash/update-ha.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 
-# Path variables
 HASS_SERVICE=${HASS_SERVICE:-home-assistant@homeassistant.service}
-VIRTUAL_ENV=${VIRTUAL_ENV:-/srv/homeassistant}
 
-# This is for log purposes
-echo
-echo "[$(date)] Update script starting"
+sudo -u homeassistant -H -s
+source /srv/homeassistant/bin/activate
+pip3 install --upgrade homeassistant
 
-# Show package information
-$VIRTUAL_ENV/bin/pip3 search homeassistant
-
-# Upgrade Home Assistant
-$VIRTUAL_ENV/bin/pip3 install --upgrade homeassistant
-
-# Restart Home Assistant
-# This requires a modification using `sudo visudo`:
-#   homeassistant ALL=(ALL) NOPASSWD: /bin/systemctl restart home-assistant.service
-# Make sure to change `home-assistant.service` to the correct service name
 sudo systemctl restart "$HASS_SERVICE"
