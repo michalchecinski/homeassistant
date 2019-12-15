7d69a27bf48cb58ed04d270a7737f0a6244b4e23
Michał Chęciński <michal.checinski@outlook.com>
2019-12-15 22:44:25 +0100

    Disable travis automatioc pulls

--- bash/update-ha.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo systemctl stop home-assistant@homeassistant.service
-sudo -u homeassistant -H -s
-source /srv/homeassistant/bin/activate
-pip3 install --upgrade homeassistant
-exit
-sudo systemctl start home-assistant@homeassistant.service
