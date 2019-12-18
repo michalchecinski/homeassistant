1e184cfecec16bc9a7cdbe219cfd252e615ef188
Michał Chęciński <michal.checinski@outlook.com>
2019-12-18 20:56:44 +0100

    update docker update

--- scripts/docker-update.sh
+++ scripts/docker-update.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 docker-compose rm -f --remove-orphans
+
+docker container prune -f
+docker network prune -f
+docker volume prune -f
+
 docker-compose pull
 docker-compose up --build -d
+
+docker network prune -f
+docker volume prune -f
