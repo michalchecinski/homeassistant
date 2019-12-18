05f018b9f5e4bea9662ba213d1dc858db5435229
Michał Chęciński <michal.checinski@outlook.com>
2019-12-18 22:42:59 +0100

    delete yellow color

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,7 +14,7 @@ SLACK_HOOK_URL=$(cat "$HASS_PATH/scripts/slack_webhook.txt")
 COLOR_BLUE="#1539e0"
 COLOR_GREEN="good"
 COLOR_RED="danger"
-COLOR_YELLOW="warning"
+#COLOR_YELLOW="warning"
 
 function remote_git() {
     git --git-dir "$HASS_PATH"/.git --work-tree="$HASS_PATH" "$@"
