645be08595660a8c2ddb64def57e3d1827846aa1
Michał Chęciński <michal.checinski@outlook.com>
2019-12-18 22:39:26 +0100

    post slack on build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,13 @@ HASS_BUILD_PATH='build/builds?definitions=15&statusFilter=completed&resultFilter
 HASS_PATH="/home/pi/homeassistant"
 
 # not change
-LAST_HASS_BUILD_FILEPATH="/tmp/hass_last_devops_build"
+LAST_BUILD_FILEPATH="/tmp/hass_last_devops_build"
+
+SLACK_HOOK_URL=$(cat "$HASS_PATH/scripts/slack_webhook.txt")
+COLOR_BLUE="#1539e0"
+COLOR_GREEN="good"
+COLOR_RED="danger"
+COLOR_YELLOW="warning"
 
 function remote_git() {
     git --git-dir "$HASS_PATH"/.git --work-tree="$HASS_PATH" "$@"
@@ -25,10 +31,10 @@ function should_update() {
     local saved_build_number
 
     # If the last build file doesn't exist, create it:
-    [ ! -f "$LAST_HASS_BUILD_FILEPATH" ] && touch "$LAST_HASS_BUILD_FILEPATH"
+    [ ! -f "$LAST_BUILD_FILEPATH" ] && touch "$LAST_BUILD_FILEPATH"
 
     latest_build_number=$(fetch_latest_build_number)
-    saved_build_number=$(cat "$LAST_HASS_BUILD_FILEPATH")
+    saved_build_number=$(cat "$LAST_BUILD_FILEPATH")
 
     # If the build number is the same as the last one retrieved, don't trigger
     # a build:
@@ -38,17 +44,48 @@ function should_update() {
         return
     fi
 
-    echo "$latest_build_number" > "$LAST_HASS_BUILD_FILEPATH"
+    echo "$latest_build_number" > "$LAST_BUILD_FILEPATH"
 
     echo "true"
 }
+
+function send_slack_message() {
+   local color="$2"
+   local text="$1"
+   local message
+   message="\`homeassistant/master\`: $text"
+
+   curl \
+       -X "POST" \
+     -H 'Content-Type: application/json' \
+     -d $'{ "attachments": [ { "color": "'"$color"'", "text": "'"$message"'" } ] }' \
+    "$SLACK_HOOK_URL"
+}
+
 updated="$(should_update)"
 
 if [ "$updated" == "true" ]
 then
-    remote_git pull
-    sleep 1
+    git_pull_result="$(remote_git pull > /dev/null 2>&1)"
+    if [ "$git_pull_result" == "1" ]; then
+        send_slack_message \
+            "Deployment failed due to failed Git pull on target host" \
+            "$COLOR_RED"
+        return
+    fi
+
+    saved_build_number=$(cat "$LAST_BUILD_FILEPATH")
+
+    send_slack_message \
+        "Deployment for build #$saved_build_number starting..." \
+        "$COLOR_BLUE"
+
     bash "$HASS_PATH"/scripts/build.sh
+
+    send_slack_message \
+        "Deployment for build #$saved_build_number completed" \
+        "$COLOR_GREEN"
+
     echo "Build finished"
 else
     echo "No deployment required"
