234be0e0397145d4fa29c81d2edb4d035be074af
Michał Chęciński <michal.checinski@outlook.com>
2019-12-22 15:38:13 +0100

    fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -69,7 +69,7 @@ send_slack_message \
     "Deployment for build starting..." \
     "$COLOR_BLUE"
 
-docker_recreate=$(docker_compose_changed $OLD_HEAD $NEW_HEAD)
+docker_recreate=$(docker_compose_changed "$OLD_HEAD" "$NEW_HEAD")
 
 if [ "$docker_recreate" == "true" ]
 then
