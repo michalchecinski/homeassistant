062403d2c0fa51453dd1ecfa43f805a576c2b538
Michał Chęciński <michal.checinski@outlook.com>
2019-12-27 11:43:34 +0100

    add git submodule sync to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -61,9 +61,10 @@ if [ "$git_pull_result" == "1" ]; then
     return
 fi
 
-NEW_HEAD="$(remote_git rev-parse HEAD)"
+remote_git submodule sync --recursive
+remote_git submodule update --init --recursive
 
-# saved_build_number="$1"
+NEW_HEAD="$(remote_git rev-parse HEAD)"
 
 send_slack_message \
     "Deployment for build starting..." \
