20f11e4a7d74090f5f74e17cc10df7b501ce00e1
Michał Chęciński <michal.checinski@outlook.com>
2020-02-17 18:28:57 +0100

    Fix flake bugs in ddns.py

--- scripts/ddns.py
+++ scripts/ddns.py
@@ -17,29 +17,44 @@ with open(home+'homeassistant/scripts/ddns_settings.json') as json_file:
 req = requests.get('https://api.ipify.org?format=json')
 
 currentIp = req.json()["ip"]
-lastIp = subprocess.run(['cat', home+'/lastIp'], stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
+lastIp = subprocess.run(['cat', home+'/lastIp'], stdout=subprocess.PIPE) \
+                    .stdout \
+                    .decode('utf-8') \
+                    .strip()
 
 print("Current IPv4 = " + currentIp)
 print("Previous IPv4 = " + lastIp)
 
+
 def update(zone, domain):
-    header = {"X-Auth-Key": authKey, "X-Auth-Email": email, "Content-Type":"application/json"}
-    req = requests.get("https://api.cloudflare.com/client/v4/zones/", headers=header)
+    header = {"X-Auth-Key": authKey,
+              "X-Auth-Email": email,
+              "Content-Type": "application/json"}
+    req = requests.get("https://api.cloudflare.com/client/v4/zones/",
+                       headers=header)
     zones = req.json()
     for result in zones["result"]:
         if(result["name"] == zone):
             print("Connected to zone "+result["name"])
-            req = requests.get("https://api.cloudflare.com/client/v4/zones/"+result["id"]+"/dns_records?type=A", headers=header)
+            req = requests.get("https://api.cloudflare.com/client/v4/zones/" +
+                               result["id"]+"/dns_records?type=A",
+                               headers=header)
             for dns in req.json()["result"]:
                 print("Checking dns record "+dns["name"])
                 if(dns["name"].startswith(domain)):
-                    d = json.dumps({"type":"A", "name":dns["name"], "content":currentIp})
-                    req = requests.put("https://api.cloudflare.com/client/v4/zones/"+result["id"]+"/dns_records/"+dns["id"], headers=header, data=d)
+                    d = json.dumps({"type": "A",
+                                    "name": dns["name"],
+                                    "content": currentIp})
+                    req = requests.put(
+                                "https://api.cloudflare.com/client/v4/zones/" +
+                                result["id"]+"/dns_records/"+dns["id"],
+                                headers=header, data=d)
                     print(req.json())
-                    if req.status_code == 200 :
+                    if req.status_code == 200:
                         print("Succesfully updated DNS record")
                         with open(home+"lastIp", "w") as file:
                             file.write(currentIp)
+
 
 if(currentIp == ""):
     print("No IP has been provided")
@@ -47,6 +62,7 @@ if(currentIp == ""):
 if(currentIp != lastIp):
     print("Ip has changed!")
     print("Updating DNS...")
-    update(domain, zone)
+    for z in zone:
+        update(domain, z)
 else:
     print("Ip has not been changed")
