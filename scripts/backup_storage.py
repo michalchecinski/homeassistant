96ba612e99da1495dc3029f25fd8e548f8d122a6
Michał Chęciński <michal.checinski@outlook.com>
2020-07-31 22:50:50 +0200

    Do not backup unifi

--- scripts/backup_storage.py
+++ scripts/backup_storage.py
@@ -15,7 +15,8 @@ def getListOfFiles(dirName):
         fullPath = os.path.join(dirName, entry)
         # If entry is a directory then get the list of files in this directory
         if os.path.isdir(fullPath):
-            allFiles = allFiles + getListOfFiles(fullPath)
+            if "unifi" not in fullPath:
+                allFiles = allFiles + getListOfFiles(fullPath)
         else:
             allFiles.append(fullPath)
     return allFiles
